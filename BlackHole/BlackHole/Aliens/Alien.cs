fbcfa9ea0d5c36ea3ae73f2ff5cedd4e924850ae
Kaylee <kayleercummings@gmail.com>
2016-04-15 14:18:10 -0500

    set up base files

---
+++ BlackHole/BlackHole/Aliens/Alien.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackHole.Aliens
+{
+  class Alien
+  {
+    private string _goal;
+
+    public string goal
+    {
+      get
+      {
+        return _goal;
+      }
+      set
+      {
+        _goal = value;
+      }
+    }
+
+
+    private string _species;
+
+    public string species
+    {
+      get
+      {
+        return _species;
+      }
+      set
+      {
+        _species = value;
+      }
+    }
+
+
+  }
+}
